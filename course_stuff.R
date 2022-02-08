4270978513eb1dd352f1b7a938d472ce30f91c2d
Tobias Stephan <tobias.stephan@ucalgary.ca>
2022-02-07 22:19:35 -0700

    update

--- course_stuff.R
+++ course_stuff.R
@@ -1,9 +1,10 @@
-clean_header <- function(x){
-  x <- stringr::str_replace(x, '[ ]+', "_")
-  x <- stringr::str_replace(x, pattern = '[(]', replacement = "")
-  x <- stringr::str_replace(x, pattern = '[)]', replacement = "")
-  x <- stringr::str_replace(x, ' ', "_")
-  x <- stringr::str_replace(x, ' ', "_")
+clean_header <- function(x) {
+  require(stringr)
+  x <- stringr::str_replace(x, "[ ]+", "_")
+  x <- stringr::str_replace(x, pattern = "[(]", replacement = "")
+  x <- stringr::str_replace(x, pattern = "[)]", replacement = "")
+  x <- stringr::str_replace(x, " ", "_")
+  x <- stringr::str_replace(x, " ", "_")
   x <- stringr::str_squish(x)
   return(x)
 }
@@ -23,42 +24,45 @@ clean_header <- function(x){
 #' @importFrom dplyr "%>%" filter select
 #' @importFrom stringr str_replace
 #' @importFrom readxl read_excel
-read_geochron <- function(path, method = 'U-Pb') {
+read_geochron <- function(path, method = "U-Pb") {
   require(dplyr)
-  require(stringr)
   require(readxl)
+
+  meta.header <- NULL
+
 
   if (method == "Ar-Ar") {
   }
   if (method == "(U-Th)/He") {
   }
   if (method == "Fission Track") {
-    details0 <- readxl::read_excel(path = path, sheet = 'Details', col_names = FALSE)
+    details0 <- readxl::read_excel(path = path, sheet = "Details", col_names = FALSE)
 
     details <- data.frame(t(details0$`...2`))
-    names(details) <-  clean_header(details0$`...1`)
+    names(details) <- clean_header(details0$`...1`)
 
-    meta0 <- readxl::read_excel(path = path, sheet = 'Metadata', col_names = FALSE)
+    meta0 <- readxl::read_excel(path = path, sheet = "Metadata", col_names = FALSE)
     meta <- data.frame(t(meta0$`...2`))
     names(meta) <- clean_header(meta0$`...1`)
 
     meta <- cbind(meta, details)
-    ages <- readxl::read_excel(path = path, sheet = 'Ages')
+    ages <- readxl::read_excel(path = path, sheet = "Ages")
 
-    return(list(meta=meta, tracks=ages))
-
+    return(list(meta = meta, tracks = ages))
   }
   if (method == "U-Pb") {
     # meta
-    meta0 <- readxl::read_excel(path = path,
-                                sheet = 1,
-                                skip = 7)
+    meta0 <- readxl::read_excel(
+      path = path,
+      sheet = 1,
+      skip = 7
+    )
     meta.header <- colnames(meta0)
     colnames(meta0) <- clean_header(colnames(meta0))
 
     meta <- meta0 %>%
       filter(!is.na(Country)) %>%
-      filter(Unique_ID != 'Unique ID') %>%
+      filter(Unique_ID != "Unique ID") %>%
       mutate(
         Longitude = as.numeric(Longitude),
         Latitude = as.numeric(Latitude),
@@ -69,35 +73,36 @@ read_geochron <- function(path, method = 'U-Pb') {
       )
 
     # isotopes
-    isotopes.header <- t(meta0[2,])
+    isotopes.header <- t(meta0[2, ])
     rownames(isotopes.header) <- NULL
     isotopes.header[1] <- "Sample ID"
     isotopes.header[2] <- "Unique ID"
-    #isotopes.header <- na.omit(isotopes.header)
     isotopes.header <-
-      stringr::str_replace(isotopes.header, '[ ]', "_")
+      stringr::str_replace(isotopes.header, "[ ]", "_")
     isotopes.header <-
       stringr::str_replace(isotopes.header, "/", ".")
     isotopes.header <-
-      stringr::str_replace(isotopes.header, '[ ]', "_")
+      stringr::str_replace(isotopes.header, "[ ]", "_")
 
     isotopes0 <-
-      readxl::read_excel(path = path,
-                         sheet = 1,
-                         skip = 7)
+      readxl::read_excel(
+        path = path,
+        sheet = 1,
+        skip = 7
+      )
     colnames(isotopes0) <- isotopes.header
 
 
     isotopes <- isotopes0[FALSE, ]
     for (i in seq_along(isotopes0$Sample_ID)) {
       if (!is.na(isotopes0$Sample_ID[i]) &
-          !is.na(isotopes0$Unique_ID[i])) {
+        !is.na(isotopes0$Unique_ID[i])) {
         sample_id <- isotopes0$Sample_ID[i]
         unique_id <- isotopes0$Unique_ID[i]
       }
       if (is.na(isotopes0$Sample_ID[i]) &
-          is.na(isotopes0$Unique_ID[i])) {
-        isotopes.i <- isotopes0[i,]
+        is.na(isotopes0$Unique_ID[i])) {
+        isotopes.i <- isotopes0[i, ]
         isotopes.i$Sample_ID <- sample_id
         isotopes.i$Unique_ID <- unique_id
 
@@ -114,7 +119,7 @@ read_geochron <- function(path, method = 'U-Pb') {
         `207.206_Age` = as.numeric(`207.206_Age`),
         `207.206_Age_Error` = as.numeric(`207.206_Age_Error`),
         `Pb*.Pbc` = as.numeric(`Pb*.Pbc`),
-        Rho  = as.numeric(Rho),
+        Rho = as.numeric(Rho),
         Rho_Error = as.numeric(Rho_Error),
         `206.238` = as.numeric(`206.238`),
         `206.238_Error` = as.numeric(`206.238_Error`),
@@ -139,7 +144,7 @@ read_geochron <- function(path, method = 'U-Pb') {
       "Sample_ID", "Unique_ID", "Fraction_ID",
       "t.Pb206U238", "st.Pb206U238",
       "t.Pb207U235", "st.Pb207U235",
-      "t.Pb207Pb206",  "st.Pb207Pb206",
+      "t.Pb207Pb206", "st.Pb207Pb206",
       "PbPb.cor", "rho", "s.rho",
       "Pb206U238", "errPb206U238",
       "Pb206Pb204", "Pb208Pb206", "U", "ThU",
@@ -155,24 +160,19 @@ read_geochron <- function(path, method = 'U-Pb') {
 }
 
 # color palette ####
-colorblind <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", '#7555C8', '#7EF791', '#85112A', '#2BE6BE', '#EE599F', '#5F9143', '#759301') # Bang Wong
+colorblind <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#7555C8", "#7EF791", "#85112A", "#2BE6BE", "#EE599F", "#5F9143", "#759301") # Bang Wong
 
-
-geochron_to_dz <- function(x){
+geochron_to_dz <- function(x) {
   lst <- list()
-  for(i in x$Sample_ID){
-    x.i <- subset(x, x$Sample_ID==i)
+  for (i in x$Sample_ID) {
+    x.i <- subset(x, x$Sample_ID == i)
     ages <- c(x.i$t)
-    #names(ages) <- i
-    lst[[i]] = ages
+    lst[[i]] <- ages
   }
 
   class(lst) <- "detritals"
   return(lst)
 }
-
-
-
 
 #' @title swathR v1.0.1
 #' @author V. Haburaj
@@ -182,18 +182,18 @@ geochron_to_dz <- function(x){
 #' specified, see arguments \code{'k'} and \code{'dist'}. Values of the swath-profile are
 #' extracted from a given raster file, see argument \code{'raster'}. CRS of raster
 #' and points have to be the same.
-#'@param coords matrix(ncol=2, nrow=2) with x and y coordinates of beginning and
+#' @param coords matrix(ncol=2, nrow=2) with x and y coordinates of beginning and
 #' end point of the baseline; each point in one row
 #' {\describe
 #' \item{column 1}{xcoordinates}
 #' \item{column 2}{ycoordinates}
 #' }
-#'@param raster raster file (loaded with \code{raster()} from package 'raster')
-#'@param k integer; number of lines on each side of the baseline
-#'@param dist numeric; distance between lines
-#'@param crs string; CRS
-#'@param method = string; method for extraction of raw data, see
-#'\code{extract()} from package 'raster': default value: 'bilinear'
+#' @param raster raster file (loaded with \code{raster()} from package 'raster')
+#' @param k integer; number of lines on each side of the baseline
+#' @param dist numeric; distance between lines
+#' @param crs string; CRS
+#' @param method = string; method for extraction of raw data, see
+#' \code{extract()} from package 'raster': default value: 'bilinear'
 #' @import sp
 #' @import rgeos
 #' @import raster
@@ -202,76 +202,161 @@ swathR <- function(coords, raster, k, dist, crs, method) {
   require(sp)
   require(rgeos)
   require(raster)
-  message('Initializing ...')
+  message("Initializing ...")
   # set default method:
   if (missing(method)) {
-    method <- 'bilinear'
+    method <- "bilinear"
   }
   # create SpatialPoints from coords:
   spt <- SpatialPoints(coords, proj4string = CRS(crs))
   # get slope of baseline:
   m <- (ymin(spt[1]) - ymin(spt[2])) / (xmin(spt[1]) - xmin(spt[2]))
   # get slope of normal function:
-  m1 <- - (1/m)
+  m1 <- -(1 / m)
   # get slope-angle from slope:
   alpha <- atan(m)
   alpha1 <- atan(m1)
   # get deltax and deltay from pythagoras:
-  if ((alpha*180)/pi < 90 & (alpha*180)/pi > 270) {
+  if ((alpha * 180) / pi < 90 & (alpha * 180) / pi > 270) {
     deltax <- cos(alpha1) * dist
-  } else deltax <- cos(alpha1) * dist * -1
-  if ((alpha*180)/pi > 0  & (alpha*180)/pi < 180) {
+  } else {
+    deltax <- cos(alpha1) * dist * -1
+  }
+  if ((alpha * 180) / pi > 0 & (alpha * 180) / pi < 180) {
     deltay <- sqrt(dist**2 - deltax**2)
-  } else deltay <- sqrt(dist**2 - deltax**2) * -1
+  } else {
+    deltay <- sqrt(dist**2 - deltax**2) * -1
+  }
   # create empty matrix:
-  swath <- matrix(nrow = k*2+1, ncol = 8)
-  colnames(swath) <- c('distance', 'mean', 'median',
-                       'std.dev.', 'min', 'max', 'quantile(25)', 'quantile(75)')
+  swath <- matrix(nrow = k * 2 + 1, ncol = 8)
+  colnames(swath) <- c(
+    "distance", "mean", "median",
+    "std.dev.", "min", "max", "quantile(25)", "quantile(75)"
+  )
   # list for spatial lines:
   allLines <- list()
   # add baseline:
-  allLines[[k+1]] <- spLines(coords, crs=crs)
+  allLines[[k + 1]] <- spLines(coords, crs = crs)
   # set distance for baseline:
-  swath[k+1, 1] <- 0
+  swath[k + 1, 1] <- 0
   # generate k lines parallel to baseline:
   for (n in 1:k) {
     # BELOW BASELINE:
     # new points
-    cn <- matrix(nrow=2, ncol=2)
-    cn[1,] <- cbind(coords[1,1] - (deltax * n), coords[1,2] - (deltay * n))
-    cn[2,] <- cbind(coords[2,1] - (deltax * n), coords[2,2] - (deltay * n))
+    cn <- matrix(nrow = 2, ncol = 2)
+    cn[1, ] <- cbind(coords[1, 1] - (deltax * n), coords[1, 2] - (deltay * n))
+    cn[2, ] <- cbind(coords[2, 1] - (deltax * n), coords[2, 2] - (deltay * n))
     # line between points:
-    allLines[[k+1-n]] <- spLines(cn, crs=crs)
+    allLines[[k + 1 - n]] <- spLines(cn, crs = crs)
     # distance value:
-    swath[k+1-n,1] <- -1*n*dist
+    swath[k + 1 - n, 1] <- -1 * n * dist
     # ABOVE BASELINE:
     # new points
-    cn <- matrix(nrow=2, ncol=2)
-    cn[1,] <- cbind(coords[1,1] + (deltax * n), coords[1,2] + (deltay * n))
-    cn[2,] <- cbind(coords[2,1] + (deltax * n), coords[2,2] + (deltay * n))
+    cn <- matrix(nrow = 2, ncol = 2)
+    cn[1, ] <- cbind(coords[1, 1] + (deltax * n), coords[1, 2] + (deltay * n))
+    cn[2, ] <- cbind(coords[2, 1] + (deltax * n), coords[2, 2] + (deltay * n))
     # line between points:
-    allLines[[k+n+1]] <- spLines(cn, crs=crs)
+    allLines[[k + n + 1]] <- spLines(cn, crs = crs)
     # distance value:
-    swath[k+n+1, 1] <- n*dist
+    swath[k + n + 1, 1] <- n * dist
   }
   gc(verbose = FALSE)
   # get raw data:
-  message('Extracting raw data (this may take some time) ...')
-  raw.data <- sapply(allLines, FUN=function(x) {extract(raster, x, method=method)})
+  message("Extracting raw data (this may take some time) ...")
+  raw.data <- sapply(allLines, FUN = function(x) {
+    extract(raster, x, method = method)
+  })
   gc(verbose = FALSE)
   # generalise data:
-  message('Generalising data ...')
-  swath[,2] <- sapply(raw.data, function(x) {mean(x, na.rm = T)})
-  swath[,3] <- sapply(raw.data, function(x) {median(x, na.rm = T)})
-  swath[,4] <- sapply(raw.data, function(x) {sd(x, na.rm = T)})
-  swath[,5] <- sapply(raw.data, function(x) {min(x, na.rm = T)})
-  swath[,6] <- sapply(raw.data, function(x) {max(x, na.rm = T)})
-  swath[,7] <- sapply(raw.data, function(x) {quantile(x, na.rm = T)[2]})
-  swath[,8] <- sapply(raw.data, function(x) {quantile(x, na.rm = T)[4]})
+  message("Generalising data ...")
+  swath[, 2] <- sapply(raw.data, function(x) {
+    mean(x, na.rm = T)
+  })
+  swath[, 3] <- sapply(raw.data, function(x) {
+    median(x, na.rm = T)
+  })
+  swath[, 4] <- sapply(raw.data, function(x) {
+    sd(x, na.rm = T)
+  })
+  swath[, 5] <- sapply(raw.data, function(x) {
+    min(x, na.rm = T)
+  })
+  swath[, 6] <- sapply(raw.data, function(x) {
+    max(x, na.rm = T)
+  })
+  swath[, 7] <- sapply(raw.data, function(x) {
+    quantile(x, na.rm = T)[2]
+  })
+  swath[, 8] <- sapply(raw.data, function(x) {
+    quantile(x, na.rm = T)[4]
+  })
   # return results:
-  results <- list(swath=swath, data=raw.data, lines=allLines)
-  message('Operation finished successfully!')
+  results <- list(swath = swath, data = raw.data, lines = allLines)
+  message("Operation finished successfully!")
   message('Structure of results (list): "swath": swath profile data (matrix, numeric), "data": raw data (list, numeric), "lines": generated lines (list, spLines)')
   gc(verbose = FALSE)
   return(results)
 }
+
+
+
+#' Elevation profile
+#'
+#' Extracts the minimum and maximum elevation data along a swathR profile.
+#'
+#' @param x list. an return object from \code{swathR}
+#' @return data.frame
+#' @import dplyr
+#' @export
+swath_profile <- function(x) {
+  elevs <- c()
+  center <- as.character(median(seq_along(x$data)))
+  for (i in seq_along(x$data)) {
+    elevs <- cbind(elevs, x$data[[i]])
+  }
+  elevs.df <- as.data.frame(elevs)
+  names(elevs.df) <- seq_along(x$data)
+
+  elevs.df <- elevs.df %>% rowwise() %>% mutate(min = min(c_across()),
+                                                max = max(c_across())) %>%
+    ungroup()
+  elevs.df <-
+    data.frame(seq_along(elevs.df$min),
+               elevs.df[center],
+               elevs.df$min,
+               elevs.df$max)
+  names(elevs.df) <- c('distance', 'elevation', 'min', 'max')
+  return(elevs.df)
+}
+
+
+
+#' @title Orthodromic distance
+#' @description The great-circle distance or orthodromic distance is the
+#' shortest distance between two points on the surface of a sphere,
+#' measured along the surface of the sphere (as opposed to a straight line
+#' through the sphere's interior)
+#' @param a lon, lat coordinate of point 1
+#' @param b lon, lat coordinate of point 2
+#' @return distance in km
+#' @export
+#' @examples
+#' berlin <- c(52.517, 13.4)
+#' tokyo <- c(35.7, 139.767)
+#' orthodrome(berlin, tokyo)
+orthodrome <- function(a, b){
+  require(pracma)
+  delta <- pracma::acosd(
+    pracma::sind(a[2]) * pracma::sind(b[2]) +
+      pracma::cosd(a[2]) * pracma::cosd(b[2]) * pracma::cosd(b[1]-a[1])
+  )
+
+  d <- 6371.00887714 * pracma::deg2rad(delta)
+
+  return(d)
+}
+
+
+
+#styler:::style_active_file()
+#lintr::lint("course_stuff.R")
